9db6723815697bf49ed92c685fbb3a225245eb70
Emily Hill <emhillmed2021@gmail.com>
2022-10-19 15:26:06 -0500

    started create new note function and app.post to set id and add note to json file and notes array

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const express = require('express')
 const app = express(); 
+const {notes} = require('./db/db.json')
 
 // 1) db.json used to store and retrieve notes using fs
 // 2) HTML routes created:
@@ -13,3 +14,29 @@ const app = express();
 //4) BONUS: add delete route 
     //a. DELETE /api/notes/:id should receive query parameter containing id of note. 
         //i) In order to delete, need to read all noties from db.json, remove given id, then rewrite notes to db.json 
+
+//1)
+
+function createNewNote(body, notesArray) {
+    //body meaning request body 
+    //notesArray has same information as {notes} 
+    console.log(body)
+    const note = body; 
+    notesArray.push(note);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({notes: notesArray}, null, 2));
+
+    return body; 
+}
+
+//3b) 
+app.post('/api/notes', (req, res) => {
+    //req.body is where incoming content will be 
+    // set id based on what the next index of array will be 
+    req.body.id = notes.length.toString();
+
+    // add note to json file and notes array 
+    const note = createNewNote(req.body, notes);
+
+    res.json(req.body); 
+})
