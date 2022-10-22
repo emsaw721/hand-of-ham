edf3a321acb8a1633b88f34fa4c866b579ac25f7
Emily Hill <emhillmed2021@gmail.com>
2022-10-22 14:31:30 -0500

    tried to make separate routs files, but it broke everthing

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,11 @@
 const fs = require('fs')
-const express = require('express')
-const path = require('path');
-const PORT= process.env.PORT || 3001; 
-const app = express(); 
 const notes = require('./db/db.json');
 const notesArr = []
-// use for deleting the note, app.delete 
-const { v4: uuidv4 } = require('uuid'); 
-
-
+const express = require('express')
+const PORT= process.env.PORT || 3001; 
+const app = express(); 
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 app.use(express.static(`public`))//before express, anything in public to root level 
@@ -26,62 +23,6 @@ app.use(express.static(`public`))//before express, anything in public to root le
         //i) In order to delete, need to read all noties from db.json, remove given id, then rewrite notes to db.json 
 
 
-
-//2b) kind of, need to have *
-app.get('/', (req,res) => {
-    console.log('Reached route'); 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
-//2a) 
-app.get('/notes', (req,res) => {
-    console.log('Reached route!')
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
-
-//3a)
-app.get('/api/notes', (req,res) => {
-   res.json(`${req.method} request received to get notes`)
-   console.info(`${req.method} request received to get notes`)
-  
-})
-
-//3b) 
-app.post('/api/notes', (req, res) => {
-console.info(`${req.method} request received to add a new note.`)
-//req.body is where incoming content will be 
-    const { title, text } = req.body;
-    console.log(req.body)
-
-    if(title && text){
-        const newNote = {
-            title,
-            text,
-            note_id: uuidv4()
-        };
-        
-        const noteString = JSON.stringify(newNote); 
-        notesArr.push(noteString)
-        // notesArr.forEach(createNewNote(noteString))
-
-        for(i=0; i< notesArr.length; i++) {
-            fs.writeFile(`./db/db.json`, notesArr[i], (err) =>
-                err
-                ? console.error(err)
-                : console.log(
-                `A new note has been written to JSON file.`
-            ))
-
-            const response = {
-            status: 'success',
-            body: newNote 
-            }
-
-        console.log(response)
-        return res.json(response)
-        }
-}
-})
-
 // function createNewNote() {
 // fs.writeFile(`./db/db.json`,notesArr[i] , (err) =>
 // err
@@ -98,28 +39,6 @@ console.info(`${req.method} request received to add a new note.`)
 // return res.json(response)
 
 // }
-
-app.delete('/api/notes/:note_id', (req,res) => {
-    if(req.body && req.params.note_id) {
-        const noteID = req.params.note_id;
-        for(let i=0; i<notesArr.length; i++){
-            const currentNote = notesArr[i];
-
-            if(currentNote.note_id === noteID){
-                let newNotes = notesArr.splice(currentNote,1);
-                let newString = JSON.stringify(newNotes)
-                fs.writeFile(`./db/db.json`, newString, (err) =>
-                err
-                ? console.error(err)
-                : console.log(
-                    `A new note has been written to JSON file.`
-                ))
-
-            }
-        }
-    } 
-    
-})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`); 
